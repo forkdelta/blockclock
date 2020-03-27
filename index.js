f8b4607dd07888b0c8fb5c1c1d05e02948e9787d
Arseniy Ivanov <freeatnet@freeatnet.com>
2020-03-27 12:56:33 +0300

    Add a background checker, terminate process if block numbers get out of sync

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ const ethers = require('ethers');
 const { send } = require('micro');
 
 const wsProvider = new ethers.providers.WebSocketProvider(process.env.WS_PROVIDER_URL);
+const etherscanProvider = new ethers.providers.EtherscanProvider(process.env.ETHERSCAN_NETWORK, process.env.ETHERSCAN_API_KEY);
 
 let blockNumber = null;
 
-console.log('subscribing...')
+console.log('subscribing...');
 wsProvider.on('block', (newBlockNumber) => {
   console.log('new block', newBlockNumber);
   blockNumber = newBlockNumber;
 });
+
+setInterval(async () => {
+  try {
+    if (blockNumber === null) {
+      console.log('blockNumber is null, skipping background check');
+      return;
+    }
+
+    const checkBlockNumber = await etherscanProvider.getBlockNumber();
+
+    console.log('background checker sees', { checkBlockNumber, blockNumber })
+    if (checkBlockNumber - blockNumber > process.env.BLOCK_DIFF_THRESH) {
+      console.error('Block difference exceeded, exiting', { blockNumber, checkBlockNumber });
+      process.exit(1);
+    }
+  } catch (err) {
+    console.error('Uncaught error in background checker, forcing an exit', err);
+    process.exit(1);
+  }
+}, 10000);
 
 module.exports = async (req, res) => {
   if (req.url !== '/') {
